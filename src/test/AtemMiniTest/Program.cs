0faf45e6ef87ea08ab7b54f06ae599582f33e0c2
jeffvarga <jeffvarga@gmail.com>
2023-09-07 01:16:32 -0700

    Initial test harness

---
+++ src/test/AtemMiniTest/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AtemMiniTest
+{
+    using BMD=BlackmagicAtemWrapper;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMD.Switcher switcher = BMD.Discovery.Connect("");
+
+            foreach (BMD.MixEffectBlock meb in switcher.MixEffectBlocks)
+            {
+                meb.PerformFadeToBlack();
+            }
+            System.Threading.Thread.Sleep(1000);
+
+        }
+    }
+}
