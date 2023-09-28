6a60547e5777bfbca7169e15b3e2e4e098f0af71
jeffvarga <jeffvarga@gmail.com>
2023-09-27 23:30:41 -0700

    Documentation fixes

--- src/lib/BlackmagicAtemWrapper/Audio/FairlightAudioInputCollection.cs
+++ src/lib/BlackmagicAtemWrapper/Audio/FairlightAudioInputCollection.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------------
-// <copyright file="AudioMixer.cs">
+// <copyright file="FairlightAudioInputCollection.cs">
 //   Copyright (c) 2023 Jeff Varga
 //
 //   Permission is hereby granted, free of charge, to any person obtaining a copy
