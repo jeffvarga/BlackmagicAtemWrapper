96528a133926aa954c50d9639da3e636c7e27d5f
jeffvarga <jeffvarga@gmail.com>
2023-09-28 00:10:22 -0700

    Adding example1: Connecting and enumerating inputs

---
+++ examples/example1/Program.cs
@@ -0,0 +1,15 @@
+﻿using BlackmagicAtemWrapper;
+
+// Connect via USB
+Switcher switcher = Discovery.ConnectTo();
+
+// Enumerate all of the inputs
+foreach (Input input in switcher.Inputs)
+{
+    Console.WriteLine($"Input #{input.InputId}");
+    Console.WriteLine($"  Short name:      {input.ShortName}");
+    Console.WriteLine($"  Long name:       {input.LongName}");
+    Console.WriteLine($"  Port type:       {input.PortType}");
+    Console.WriteLine($"  Program tallied: {input.IsProgramTallied}");
+    Console.WriteLine($"  Preview tallied: {input.IsPreviewTallied}");
+}
