4980d9ee9af8fa1d3e22bb93cb1e8eae4a78f966
jeffvarga <jeffvarga@gmail.com>
2023-09-28 00:33:46 -0700

    Uploading an example of how to perform a transition

---
+++ examples/example2/Program.cs
@@ -0,0 +1,18 @@
+﻿using BlackmagicAtemWrapper;
+
+// Connect via USB
+Switcher switcher = Discovery.ConnectTo();
+
+// Grab the first MixerEffect block
+MixEffectBlock meb = switcher.MixEffectBlocks.First();
+
+// Set the preview input to the first input (often black)
+meb.PreviewInput = switcher.Inputs.First().InputId;
+// Set the program input to the second input (often the first physical input)
+meb.ProgramInput = switcher.Inputs.Skip(1).First().InputId;
+
+// Performs an auto transition
+meb.PerformAutoTransition();
+
+/// Wait - ATEM will stop transitioning if the API disconnects, as it would without the sleep.
+System.Threading.Thread.Sleep(1000);
